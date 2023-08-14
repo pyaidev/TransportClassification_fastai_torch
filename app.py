47bef1dc823b51f8df8babe2137bff656a3cc4f0
Nurmuhammad Mashrapov <nurmuhammad@MacBook-Pro---user.local>
2023-08-15 00:52:30 +0500

    feat: model and fastapi

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from fastai.vision.all import *
+import pathlib
+import plotly.express as px
+
+st.title('Transportni klassifikatsiya qiluvchi model')
+
+file = st.file_uploader('Rasm yuklang', type=['png', 'jpg', 'jpeg'])
+
+if file is not None:
+    st.image(file)
+    img = PILImage.create(file)
+    model = load_learner('transport_model.pkl')
+
+    pred, pred_id, probs = model.predict(img)
+    st.success(f"Bashorat: {pred}")
+    st.info(f"Extimollik: {probs[pred_id]*100:.1f}%")
+
+    flg=px.bar(x=model.dls.vocab, y=probs)
+    st.plotly_chart(flg)
