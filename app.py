45921f0ac0fd72bf40bc3fef9b086ba15849fe14
Nurmuhammad Mashrapov <112689446+nurmukhammaddev@users.noreply.github.com>
2023-08-15 11:59:57 +0500

    fix: validation

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from fastai.vision.all import *
-import pathlib
 import plotly.express as px
 
 st.title('Transportni klassifikatsiya qiluvchi model: Samalyot, Mashina yoki Qayiq')
@@ -20,18 +19,19 @@ if file is not None:
         img = PILImage.create(file)
         model = load_learner('transport_model.pkl')
 
-        pred, pred_id, probs = model.predict(img)
+        pred_class, _, probs = model.predict(img)
+        pred_class = str(pred_class)
+        pred_prob = probs[model.dls.vocab.o2i[pred_class]] * 100
 
-        if model.dls.vocab[pred_id] in expected_categories:
-            st.success(f"Bashorat: {pred}")
-            st.info(f"Extimollik: {probs[pred_id] * 100:.1f}%")
+        if pred_prob >= 90:
+            st.success(f"Bashorat: {pred_class}")
+            st.info("Extimollik: {:.1f}%".format(pred_prob))
 
             flg = px.bar(x=model.dls.vocab, y=probs)
             st.plotly_chart(flg)
 
-            st.image(img.to_thumb(300, 300), caption=f'Natija: {pred}', use_column_width=True)
+            st.image(img.to_thumb(300, 300), caption=f'Natija: {pred_class}', use_column_width=True)
         else:
-            st.error("Noto'g'ri kategoriya. Iltimos, Car, Airplane yoki Boat turlariga tegishli rasmlarni yuklang.")
+            st.error("Noto'g'ri kategoriya yoki noto'g'ri ehtimollik. Iltimos, Car, Airplane yoki Boat turlariga tegishli va ehtimollik 90% dan yuqori rasmlarni yuklang.")
 
-
-st.markdown('''Developed by Nurmuhammad Mashrapov 👨🏻‍💻‍''')
+st.markdown('''Developed by Nurmuhammad Mashrapov 👨🏻‍💻''')
